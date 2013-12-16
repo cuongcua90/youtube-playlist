31cce99062e6ec9d92801e00a1639500085bacd4
cuongcua90 <cuongcua90@hotmail.com>
2013-12-16 21:21:25 +0700

    srs pdf

--- app/scripts/services/youtubeService.js
+++ app/scripts/services/youtubeService.js
@@ -2,6 +2,13 @@
 
 var apiExternalMusic = function($http) {
     var youtubeApiUrl = "http://gdata.youtube.com/feeds/api/videos?q=";
+    var lastFmUrl = "http://ws.audioscrobbler.com/2.0/";
+    var lastFmApiKey = "eb1b9816c22aa3985aa863c03cc738cb";
+
+    var call = function(method, params, keyword){
+      var url = lastFmUrl + "?method="+method;  
+
+    }
 
     var musicService = {};
   	musicService.searchYoutube = function(keyword){
@@ -16,6 +23,12 @@ var apiExternalMusic = function($http) {
             params: params
         });
   	}	
+
+
+
+    musicService.searchLastFmTrackInfo = function(keyword){
+      var url = lastFmUrl + "?method=";
+    }
 
   	return musicService;
 };
